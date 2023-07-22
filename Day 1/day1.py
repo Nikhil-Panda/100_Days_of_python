2b324dd7c901edbe1d4419acfebc4756a2fb18f1
Nikhil-Panda <pandanikhil0408@gmail.com>
2023-07-22 15:38:09 +0530

    Day 1

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,3 +1,8 @@
 print("hello "+ input('what is your name?')+"!")
 #variable names
 #input function above
+
+# print('what's up?') syntax error because the indentation 
+# for the print fn ends at what's 
+# to avoid this
+print("What's your name?")
