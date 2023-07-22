5db81008f35f652a408c76ca2205d8a98f263607
Nikhil-Panda <pandanikhil0408@gmail.com>
2023-07-22 15:35:50 +0530

    Day 1

---
+++ Day 1/bandname.py
@@ -0,0 +1,5 @@
+#basic name generator for bands inn reference to their pets and their birthplace
+city_name = input('What city did you grow up in ?\n')
+pet_name = input("What is your pet's name?\n")
+
+print("Your band name could be "+city_name+" "+pet_name)
---
+++ Day 1/day1.py
@@ -0,0 +1,3 @@
+print("hello "+ input('what is your name?')+"!")
+#variable names
+#input function above
