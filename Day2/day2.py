bf7d9688989d6abd17a6912401f34baa27c12218
Nikhil-Panda <pandanikhil0408@gmail.com>
2023-07-23 22:07:02 +0530

    Day 2

---
+++ Day2/day2.py
@@ -0,0 +1,12 @@
+val = 4474838_9738782.74837
+print(val)
+# "_" in integer types are ignored 
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
+# #spaces considered as characters 
+
+print(type(len("Hello")))
+#keep an eye for type errors and solve them by setting the correct datatypes
+#int(), float(), str(), type() functions to set the datatypes straight
+#PEMDAS
